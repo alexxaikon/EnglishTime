362d06d75b243419e2894874acef1f7c43c9c4fd
Alexander <alex.xaikon@gmail.com>
2017-06-25 13:46:14 +1000

    Install SQLite

---
+++ EngishTime/EngishTime.Android/Infrastructure/SQLite_Android.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System.IO;
+using Xamarin.Forms;
+using EngishTime.Infrastructure;
+using EngishTime.Droid.Infrastructure;
+
+[assembly: Dependency(typeof(SQLite_Android))]
+namespace EngishTime.Droid.Infrastructure
+{
+    public class SQLite_Android : ISQLite
+    {
+        public SQLite_Android() { }
+        public string GetDatabasePath(string sqliteFilename)
+        {
+            string documentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            var path = Path.Combine(documentsPath, sqliteFilename);
+            return path;
+        }
+    }
+}
---
+++ EngishTime/EngishTime.iOS/Infrastructure/SQLite_iOS.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using System.IO;
+using EngishTime.Infrastructure;
+using EngishTime.iOS.Infrastructure;
+
+[assembly: Dependency(typeof(SQLite_iOS))]
+namespace EngishTime.iOS.Infrastructure
+{
+
+    public class SQLite_iOS : ISQLite
+    {
+        public SQLite_iOS() { }
+        public string GetDatabasePath(string sqliteFilename)
+        {
+            // определяем путь к бд
+            string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            string libraryPath = Path.Combine(documentsPath, "..", "Library"); // папка библиотеки
+            var path = Path.Combine(libraryPath, sqliteFilename);
+
+            return path;
+        }
+    }
+}
